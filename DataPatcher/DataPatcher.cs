1fb1ef45153768b24c66b574d176c40332214e07
Terrance Codd <tcodd86@msn.com>
2013-09-04 10:05:00 -0400

    Initial commit for DataPatcher project

---
+++ DataPatcher/DataPatcher.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataPatcher
+{
+    public partial class DataPatcher : Form
+    {
+        public DataPatcher()
+        {
+            InitializeComponent();
+        }
+
+        string filePath;
+        List<string[]> dataFiles = new List<string[]>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] dataFile = {};
+            addDataFile.Title = "Select a data file to patch";
+            if (String.IsNullOrEmpty(filePath))
+            {
+                addDataFile.InitialDirectory = "C:\\";
+            }
+            else
+            {
+                addDataFile.InitialDirectory = filePath;
+            }
+            addDataFile.FileName = "";
+            addDataFile.Filter = "Text|*.txt|All Files|*.*";
+
+            addDataFile.ShowDialog();
+            filePath = addDataFile.FileName;
+
+            using (StreamReader dataReader = new StreamReader(filePath))
+            {
+                char[] delimiters = new char[] { '\t', '\r' };
+                string nextLine;
+                while ((nextLine = dataReader.ReadLine()) != null)
+                {
+                    string[] dataLine = nextLine.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                    dataFile.Concat(dataLine).ToArray();
+                }
+                dataFiles.Add(dataFile);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var decData = new List<decimal[]>();
+            decimal[] tempDec;
+            for (int i = 0; i < dataFiles.Count; i++)
+            {
+                tempDec = new decimal[dataFiles[i].Length];
+                for (int j = 0; j < dataFiles[i].Length; j++)
+                {
+                    tempDec[j] = Convert.ToDecimal(dataFiles[i][j]);
+                }
+                decData.Add(tempDec);
+            }
+        }//end open data file button
+
+        private List<Segment> segmenter(List<decimal[]> data)
+        {
+            var segList = new List<Segment>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                //find average step size based off of first 10 data points
+                decimal diff = 0.0M;
+                //need to use step of 2 and start at 1 because every other data point is presumed to be a frequency point
+                for (int k = 1; k < 22; k += 2)
+                {
+                    diff += data[i][k + 2] - data[i][k];
+                }
+                //divide by 10 to get the average step between data points.
+                diff /= 10.0M;                
+                //now multiply by 4 and set that as the threshold for making a new segment
+                diff *= 4.0M;
+                //check to make sure that diff is a positive value
+                diff = Math.Abs(diff);
+
+                decimal[] temp;
+
+                //to store where the next segment should start at
+                int start = 0;
+                for (int j = 3; j < data[i].Length; j += 2)
+                {
+                    if (Math.Abs(data[i][j] - data[i][j - 2]) > diff)
+                    {
+                        //need to add code here to take contiguous portion of data from the data[i] array to pass to segment constructor
+                        start = j - 3;
+                    }
+                }
+            }
+
+
+            return segList;
+        }//end method segmenter
+
+        private decimal[] patcher(List<Segment> segList)
+        {
+            decimal[] patchedData;
+
+
+            return patchedData;
+        }
+    }
+}
---
+++ DataPatcher/Segment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPatcher
+{
+    class Segment
+    {
+        public decimal start { get; private set; }
+        public decimal end { get; private set; }
+
+        //private data member to store the actual data from the segment
+        private decimal[,] data;
+        /// <summary>
+        /// Method to return data array.
+        /// </summary>
+        /// <returns>
+        /// Array data.
+        /// </returns>
+        public decimal[,] getData()
+        {
+            return data;
+        }
+        /// <summary>
+        /// Returns the frequency at point i
+        /// </summary>
+        /// <param name="index">
+        /// Index to return
+        /// </param>
+        /// <returns>
+        /// Value in data array at data point i
+        /// </returns>
+        public decimal getData(int i)
+        {
+            return data[i, 0];
+        }
+
+        /// <summary>
+        /// Constructor for Segment object.
+        /// </summary>
+        /// <param name="segData">
+        /// One dimensional array containing frequency and absorption values.
+        /// </param>
+        public Segment(decimal[] segData)
+        {
+            data = new decimal[segData.Length / 2, 2];
+            for (int i = 0; i < segData.Length; i++)
+            {
+                data[i / 2, i % 2] = segData[i];
+            }
+            start = segData[0];
+            end = segData[segData.Length - 2];
+        }
+    }
+}
